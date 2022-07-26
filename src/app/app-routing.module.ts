8fc0f5430825bfc0b7e2fd1102f5e2aa48d793ec
Sherin Mathew <jeel@ifourtechnolab.com>
2022-07-26 10:08:58 +0530

    feat : general app settings

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CounterComponent } from './counter/counter/counter.component';
+import { HomeComponent } from './home/home.component';
+import { AddPostComponent } from './posts/add-post/add-post.component';
+import { EditPostComponent } from './posts/edit-post/edit-post.component';
+import { PostsListComponent } from './posts/posts-list/posts-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component:HomeComponent,
+  },
+  {
+    path:'counter',
+    loadChildren:()=>import('./counter/counter.module').then(m=>m.CounterModule)
+  },
+  {
+    path:'posts',
+    loadChildren:()=>import('./posts/posts.module').then(m=>m.PostModule),
+  },
+  {
+    path:'auth',
+    loadChildren:()=>import('./auth/auth.module').then(m=>m.AuthModule), 
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,22 +7,31 @@ import { CounterComponent } from './counter/counter/counter.component';
 import { CounterOutputComponent } from './counter/counter-output/counter-output.component';
 import { CounterButtonsComponent } from './counter/counter-buttons/counter-buttons.component';
 import { StoreModule } from '@ngrx/store';
-import {counterReducer} from './counter/state/counter.reducer';
 import { CustomCounterInputComponent } from './counter/custom-counter-input/custom-counter-input.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { PostsListComponent } from './posts/posts-list/posts-list.component';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from 'src/environments/environment';
+import { appReducer } from './store/app.state';
+import { AddPostComponent } from './posts/add-post/add-post.component';
+import { EditPostComponent } from './posts/edit-post/edit-post.component';
 @NgModule({
   declarations: [
     AppComponent,
-    CounterComponent,
-    CounterOutputComponent,
-    CounterButtonsComponent,
-    CustomCounterInputComponent
+    HomeComponent,
+    HeaderComponent,
+   
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    StoreModule.forRoot({counter:counterReducer}),
-    FormsModule
+    FormsModule,
+    StoreModule.forRoot({}),
+    StoreDevtoolsModule.instrument({ 
+      maxAge: 25,
+      logOnly: environment.production }),
   ],
   providers: [],
   bootstrap: [AppComponent]
